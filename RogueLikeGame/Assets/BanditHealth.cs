eea9b2c3461dfdd9478cc29ff5c2ba2a6716077d
Sean Pashaev <sp33287@uga.edu>
2024-12-18 14:52:55 -0500

    Added health to bandit and method which allows the player to hit the enemy bandit and kill them

---
+++ RogueLikeGame/Assets/BanditHealth.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections;
+
+
+public class BanditHealth : MonoBehaviour
+{
+    private int health = 2;
+
+    private Animator animator;
+
+    private BanditBehavior bandit;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        GameObject banditObject = GameObject.Find("Bandit");
+        if (banditObject != null)
+        {
+            bandit = banditObject.GetComponent<BanditBehavior>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HurtBandit() {
+        health--;
+        animator.SetTrigger("Hurt");
+        if (health <= 0) {
+            StartCoroutine(Die());
+        }
+    }
+    IEnumerator Die() {
+        animator.SetTrigger("Death");
+        yield return new WaitForSeconds(.5f);
+        Destroy(bandit.gameObject);
+    }
+}
+
--- RogueLikeGame/Assets/PlayerActions.cs
+++ RogueLikeGame/Assets/PlayerActions.cs
@@ -6,22 +6,36 @@ public class PlayerActions : MonoBehaviour
 
     private Animator animator;
 
+    private PlayerDistance playerDistance;
+
+    private bool isBlocking = false;
+    private bool isAttacking = false;
     void Start()
     {
         animator = GetComponent<Animator>();
+        playerDistance = GetComponent<PlayerDistance>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if (Input.GetMouseButton(0)) {
-             animator.SetTrigger("Attack1");
+        if (Input.GetMouseButton(0) && !isAttacking) {
+            isAttacking = true;
+            animator.SetTrigger("Attack1");
         }
-        if (Input.GetMouseButton(1)) {
+
+        
+        if (Input.GetMouseButton(1) && !isBlocking) {
+            isBlocking = true;
              animator.SetTrigger("Block");
-        }
         
+        }
     }
-    
+    void endOfAttack() {
+        isAttacking = false;
+    }
+    void endOfBlock() {
+        isBlocking = false;
+    }
 }
